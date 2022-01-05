f3e2f76a73e3ffa1ebac5dda9237e33c9a95385b
Alexander Karrer <alexander.karrer@tractive.com>
2022-01-05 13:47:23 +0100

    Initial XCode Project creation

---
+++ ios_app/CLC_Passport/CLC_Passport/CLC_PassportApp.swift
@@ -0,0 +1,17 @@
+//
+//  CLC_PassportApp.swift
+//  CLC_Passport
+//
+//  Created by Alexander Karrer on 05.01.22.
+//
+
+import SwiftUI
+
+@main
+struct CLC_PassportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
